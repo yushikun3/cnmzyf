d6974edccbe81232fea11cda382e5cf963adc6e6
yushikun3 <49387860+yushikun3@users.noreply.github.com>
2020-02-07 15:11:01 +0800

    创建java文件
    第一次创建java文件

---
+++ test.java
@@ -0,0 +1,94 @@
+public class BusInfoConstant {
+
+	/**
+	 * 万卡商城请求贷款申请信息查询接口/qm-timmer/timer/queryLoanInfo时传source
+	 */
+	public static final String WKSC_SOURCE_DEFAULT = "wksc";
+	
+	/**
+	 * 帮你还请求贷款申请信息查询接口appId时传source
+	 */
+	public static final String BNH_SOURCE_DEFAULT = "bnh";
+	
+	/**
+	 * bus渠道ID，数据字典
+	 */
+	public static final String BUS_CHANNELID_DEFAULT = "1056";
+	
+	/**
+	 * bus渠道版本
+	 */
+	public static final String BUS_CHANNEL_VERSION = "V1.0";
+	
+	/**
+	 * bus对应系统编码
+	 */
+	public static final String BUS_SYSCODE = "502";
+	
+	/**
+	 * 征信信息收集接口
+	 */
+	public static final String BUS_INTERFACECODE_CREDIT = "50202";
+	
+	/**
+	 * 客户信息收集接口
+	 */
+	public static final String BUS_INTERFACECODE_CUSTOMER = "50203";
+	
+	/**
+	 * 贷款申请收集接口
+	 */
+	public static final String BUS_INTERFACECODE_LOANORDER = "50206";
+	
+	/**
+	 * 文件上传接口
+	 */
+	public static final String BUS_INTERFACECODE_UPLOAD = "50207";
+	
+	/**
+	 * 商城ProductOwner字段:万卡
+	 */
+	public static final String PRODUCTOWNER_TYPE_ONECARD = "1";
+	
+	/**
+	 * 商城ProductOwner字段:合缘
+	 */
+	public static final String PRODUCTOWNER_TYPE_HY = "2";
+	
+	/**
+	 * 工单后台
+	 */
+	public static final String MSG_FROM_ORDER_CENTER = "order_center";
+	
+	/**
+	 * 帮你还
+	 */
+	public static final String MSG_FROM_REFUND = "refund";
+	
+	/**
+	 * 商城
+	 */
+	public static final String MSG_FROM_MALL = "mall";
+	
+	/**
+	 * 叮当
+	 */
+	public static final String MSG_FROM_DORAEMON = "doraemon";
+	
+	/**
+	 * 推bus对应的账户类型:银行卡
+	 */
+	public static final String ACCOUNT_TYPE_BANK_CARD = "B4601";
+	
+	/**
+	 * 推bus对应的账户类型:金账户
+	 */
+	public static final String ACCOUNT_TYPE_GOLDEN = "B4602";
+	
+	/**
+	 * 账户属性（存管1、存管2、托管、实体卡），数据字典B134:实体卡
+	 */
+	public static final String ACCOUNT_TRUSTEETYPE_ENTITY = "B134003";
+	
+	
+}
